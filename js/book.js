0f7e3592c182b735e5178daa31a179deaa360366
donghylee <2donghy@gmail.com>
2026-09-02 18:03:23 +0900

    2026.09.02last

--- js/book.js
+++ js/book.js
@@ -1,56 +1,77 @@
- async function fetchBooks(query) {
-            const params = new URLSearchParams({
-                target: "title",
-                query,
-                size: 8
-            });
-            const url = `https://dapi.kakao.com/v3/search/book?${params}`;
+async function fetchBooks(query) {
+    const params = new URLSearchParams({
+        target: "title",
+        query,
+        size: 4
+    });
+    const url = `https://dapi.kakao.com/v3/search/book?${params}`;
 
-            const response = await fetch(url, {
-                method: 'GET',
-                headers: {
-                    Authorization: "KakaoAK e7d46386f29140ff8e68d2efe5d8b802"
-                }
-            });
-
-            if (!response.ok) {
-                throw new Error(`HTTP 오류: ${response.status}`);
-            }
-
-            return response.json();
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            Authorization: "KakaoAK e7d46386f29140ff8e68d2efe5d8b802"
         }
+    });
 
-        async function bookData() {
-            try {
-                // query와 section ID를 매핑
-                const queries = [
-                    // { query: "자바스크립트", sectionId: "new" },
-                    { query: "정원", sectionId: "sale" }
-                ];
+    if (!response.ok) {
+        throw new Error(`HTTP 오류: ${response.status}`);
+    }
 
-                for (const { query, sectionId } of queries) {
-                    const data = await fetchBooks(query);
+    return response.json();
+}
 
-                    // 해당 섹션 내의 .box 요소 8개 선택
-                    const section = document.querySelector(`#${sectionId}`);
-                    // const boxElements = section.querySelectorAll(".box");
+async function bookData() {
+    try {
+        // query와 section ID를 매핑
+        const queries = [
+            { query: "정원", sectionClass: "s-middle" }
+        ];
 
-                    boxElements.forEach((box, i) => {
-                        const doc = data.documents[i];
-                        if (!doc) return;
+        for (const { query, sectionClass } of queries) {
+            const data = await fetchBooks(query);
 
-                        // 요소 생성 및 추가
-                        box.innerHTML = `<img src="${doc.thumbnail}">
-                        <h3>${doc.title}</h3>
-                        <h6>${doc.authors}</h6>
-                        <p>${doc.contents.substring(0, 60)}</p>
-                        <button>click</button>
+            // 해당 섹션 내의 .box 요소 8개 선택
+            const section = document.querySelector(`.${sectionClass}`);
+            const boxElements = section.querySelectorAll(".swiper-slide");
+
+            boxElements.forEach((box, i) => {
+                const doc = data.documents[i];
+                if (!doc) return;
+
+                // 요소 생성 및 추가
+                box.innerHTML = `<div class="m-t-img"><img src="${doc.thumbnail}"></div>
+                <div class="m-t-text">
+                  <div class="m-t-text2">${doc.title}</div>
+                  <div class="m-t-text3">${doc.contents}</div>
+                  
+                  <div class="m-t-text5">${doc.author} 저 | ${doc.publisher}</div>
+                  <div class="m-t-text6"><span class="price-num">${Math.round(doc.price*0.9)}원</span> (10% 할인) 🅿️ ${Math.round(doc.price*0.1)}</div>
+                </div>
                         `
-                    });
-                }
-            } catch (error) {
-                console.error('에러 발생:', error);
-            }
-        }
+            });
 
-        bookData();
+            // 첫번째 섹션 두번째 스와이프
+
+            var swiper3 = new Swiper('.mySwiper3', {
+                spaceBetween: 30,
+                effect: 'fade',
+                navigation: {
+                    nextEl: '.mySwiper3 .swiper-button-next',
+                    prevEl: '.mySwiper3 .swiper-button-prev',
+                },
+                pagination: {
+                    el: document.querySelector('.middle-t .swiper-pagination'),
+                    clickable: true,
+                    renderBullet: function (index, className) {
+                        return `<img class="${className}" alt="bookimg" src="${data.documents[index].thumbnail}">`;
+                    },
+                },
+                
+            });
+        }
+    } catch (error) {
+        console.error('에러 발생:', error);
+    }
+}
+
+bookData();
